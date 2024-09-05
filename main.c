c68009b14eb571af199f77e7abef2c46c0f2796f
bhavana579 <152296114+bhavana579@users.noreply.github.com>
2024-09-05 14:40:10 +0530

    Add files via upload
    my first file

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "header.h"
+void main()
+{
+printf("\nI am in main.c\n");
+printf("\ncalling hello function");
+hello();
+printf("\nCalling add function");
+add(4,5);
+printf("\nBack to main function");
+}
+
