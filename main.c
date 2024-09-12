991208a87c5c3b4990f95adeafcd827a0f2c59d9
sahithii08 <erukullasahithi@gmail.com>
2024-09-12 14:49:01 +0530

    my first commit from cmd prompt

--- main.c
+++ main.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-#include "header.h"
-void main()
-{
-printf("\nI am in main.c\n");
-printf("\ncalling hello function");
-hello();
-printf("\nCalling add function");
-add(4,5);
-printf("\nBack to main function");
-}
-
+hiii bhavana
